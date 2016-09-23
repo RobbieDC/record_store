08de6f6c499c758572686b9aeaf0836ad502e367
Robbie Campbell <rcampbell182@gmail.com>
2016-09-23 11:49:33 +0100

    set up store constructor

--- specs/store_spec.js
+++ specs/store_spec.js
@@ -10,7 +10,7 @@ describe("store", function() {
   });
 
   it("store has name", function() {
-    assertt.equal( "Bieber & Beyond", store1.name );
+    assert.equal( "Bieber & Beyond", store1.name );
   });
 
 });
