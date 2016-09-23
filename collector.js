09172ef6b9d36089354dabb468909ff6dd6f4a04
Robbie Campbell <rcampbell182@gmail.com>
2016-09-23 14:50:17 +0100

    set up and tested customer and its attributes

--- collector.js
+++ collector.js
@@ -1,1 +1,7 @@
-var asset = require("asset");
+var Collector = function( name, cash ) {
+  this.name = name;
+  this.cash = cash;
+  this.records = [];
+}
+
+module.exports = Collector;
--- specs/collector_spec.js
+++ specs/collector_spec.js
@@ -1,4 +1,4 @@
-var asset = require("asset");
+var assert = require("assert");
 var Collector = require("../collector");
 
 describe("collector", function() {
@@ -6,11 +6,19 @@ describe("collector", function() {
   var partickJBLover;
 
   beforeEach(function() {
-    partickJBLover = new Collector("Alan", 50);
+    partickJBLover = new Collector("Ralan Ussel", 50);
   });
 
   it("collector has name", function() {
-    assert.equal( "Alan", partickJBLover.name );
+    assert.equal( "Ralan Ussel", partickJBLover.name );
+  });
+
+  it("collector has cash", function() {
+    assert.equal( 50, partickJBLover.cash );
+  });
+
+  it("collector has empty record array", function() {
+    assert.deepEqual( [], partickJBLover.records );
   });
 
 });
